6d680115919e447afd86007547c5ba22fb9cd79b
Derian Paez <derianpaez01@hotmail.com>
2024-02-19 17:57:05 -0500

    chore: update favorite services to new schema

--- src/app/(home)/favoritos/page.tsx
+++ src/app/(home)/favoritos/page.tsx
@@ -14,9 +14,9 @@ export default function Favoritos() {
       <div className='grid'>
         <Card shadow='none' className='border border-divider'>
           <CardBody className='grid gap-4'>
-            {favoriteProducts.map(({ id, name, imageUrls, price }, index) => (
+            {favoriteProducts.map(({ id, name, image, price }, index) => (
               <Fragment key={id}>
-                <FavoriteItem name={name} image={imageUrls[0]} price={price} productId={id} />
+                <FavoriteItem name={name} image={image} price={price} productId={id} />
                 {index !== favoriteProducts.length - 1 && <Divider />}
               </Fragment>
             ))}
