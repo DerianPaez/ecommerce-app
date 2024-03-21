75e31583d7782561e78b4cdde745ff4880a71bcc
James Mendoza <mendozajames02@hotmail.com>
2024-03-21 18:33:35 -0500

    feat: add correct calculations in cart summary

--- src/components/cart-summary/index.tsx
+++ src/components/cart-summary/index.tsx
@@ -18,34 +18,35 @@ export default function CartSummary({
       <CardBody className='grid grid-flow-row auto-rows-max gap-2'>
         <div className='grid grid-flow-col auto-cols-max justify-between'>
           <span>Subtotal</span>
-          <span>$ {subtotal}</span>
+          <span>$ {subtotal.toFixed(2)}</span>
         </div>
 
-        <div className='grid grid-flow-col auto-cols-max justify-between items-center'>
+        {/* TODO: Active */}
+        {/* <div className='grid grid-flow-col auto-cols-max justify-between items-center'>
           <span className='text-sm text-gray-500'>Descuento</span>
           <span className='text-sm text-gray-500'>$ {discount}</span>
         </div>
 
         <div className='grid grid-flow-col auto-cols-max justify-between items-center'>
           <span className='text-sm text-gray-500'>Envio</span>
-          <span className='text-sm text-gray-500'>$ {shipping}</span>
-        </div>
+          <span className='text-sm text-gray-500'>$ {shipping.toFixed(2)}</span>
+        </div> */}
 
         <div className='grid grid-flow-col auto-cols-max justify-between items-center'>
           <span className='text-sm text-gray-500'>Impuestos</span>
-          <span className='text-sm text-gray-500'>$ {tax}</span>
+          <span className='text-sm text-gray-500'>$ {tax.toFixed(2)}</span>
         </div>
 
         <Divider className='my-2' />
 
         <div className='grid grid-flow-col auto-cols-max justify-between'>
           <span className='font-semibold'>Total</span>
-          <span className='font-semibold'>$ {total}</span>
+          <span className='font-semibold'>$ {total.toFixed(2)}</span>
         </div>
       </CardBody>
 
       <CardFooter>
-        <Button color='success' variant='flat'>
+        <Button color='success' variant='flat' isDisabled={subtotal === 0}>
           Reservar por Whatsapp
         </Button>
       </CardFooter>
