f04b30130a24f1019872458c4ec3ed4f1c831d93
James Mendoza <mendozajames02@hotmail.com>
2024-03-21 17:31:00 -0500

    feat: implement snackbar for user feedback on errors

--- src/context/global/global.context.tsx
+++ src/context/global/global.context.tsx
@@ -32,7 +32,7 @@ export const GlobalProvider = ({ children, initialState }: GlobalContextProvider
 export const useGlobal = () => {
   const context = useContext(GlobalContext)
   if (!context) {
-    throw new Error('useGlobal must be used within a ProductsProvider')
+    throw new Error('useGlobal must be used within a GlobalProvider')
   }
   return context
 }
